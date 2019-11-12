4268fc1889308f717590041c79b01cabb56f5d38
Uğur Değirmenci <ugur@ugurdegirmenci.net>
2019-11-12 14:47:28 +0300

    sql connection hide

--- UurArticle/DataAccess/Context.cs
+++ UurArticle/DataAccess/Context.cs
@@ -7,7 +7,7 @@ namespace DataAccess
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server = 77.223.142.42; Initial Catalog = UgurDegirmenci; Persist Security Info = False; User ID = uurde; Password =0734ypkEUZ87; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = True; Connection Timeout = 30;");
+            optionsBuilder.UseSqlServer(@"Server = ###; Initial Catalog = UgurDegirmenci; Persist Security Info = False; User ID = uurde; Password =###; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = True; Connection Timeout = 30;");
         }
 
         public DbSet<BlogArticle> BlogArticle { get; set; }
